ee3c25515c84a02c1643bc7a4a3a2fad4f36198a
jijiwuming <1020293664@qq.com>
2016-08-19 14:29:29 +0800

    添加项目文件。

---
+++ Movies/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Movies.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Afternoon";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestresponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestresponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Movies/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Movies.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="Please enter Your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please enter Your email address")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage ="Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter Your phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "Please specify whether Your will attend")]
+        public bool? WillAttend { get; set; }
+    }
+}
